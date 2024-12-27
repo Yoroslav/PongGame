dcd0a60878f1f9075d5eba1360951caa47422b61
Yoroslav <140841390+Yoroslav@users.noreply.github.com>
2024-12-28 00:16:52 +0200

    Base Pong game

---
+++ Game/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new PongGame();
+            game.Run();
+        }
+    }
+}
